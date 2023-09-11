a2fc65cafd09548cd9b07424efc2a96c7f9a99ef
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-11 22:36:37 +0000

    Setting Email Backend

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -154,3 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+
+# EMAIL CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
