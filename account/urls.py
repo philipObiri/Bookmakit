751832636946890558ac1b0af25178a90bf751fa
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-11 20:20:13 +0000

    Added Dashboard URL

--- account/urls.py
+++ account/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     # URL to the Built In Login / Logout views :
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 ]
