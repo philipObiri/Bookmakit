9987c5e2cc0f615136f44870721e16eade3c2e19
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-11 22:24:35 +0000

    Password URLS

--- account/urls.py
+++ account/urls.py
@@ -26,6 +26,23 @@ urlpatterns = [
         name="password_change_done",
     ),
 
+
+    # reset password urls
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), 
+    name='password_reset_done'),
+
+    path('password-reset/<uidb64>/<token>/',
+    auth_views.PasswordResetConfirmView.as_view(),
+    name='password_reset_confirm'),
+
+    path('password-reset/complete/',
+    auth_views.PasswordResetCompleteView.as_view(),
+    name='password_reset_complete'),
+
+    
+
      # Dashboard URL
     path("", views.dashboard, name="dashboard"),
 ]
