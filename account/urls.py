f2488c6035069b84012cc8c28ae29d47306be3e6
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-11 20:19:34 +0000

    Added Logout URL

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 
     # URL to the Built In Login / Logout views :
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path('', views.dashboard, name='dashboard'),
 ]
