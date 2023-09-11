ea7cd6056745c2d856e844d345a9dc09678f80f2
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-11 22:17:29 +0000

    Added URLs for changing user password

--- account/urls.py
+++ account/urls.py
@@ -7,8 +7,25 @@ urlpatterns = [
     # URL to the function based login view :
     # path("login/", views.user_login, name="login"),
 
-    # URL to the Built In Login / Logout views :
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
+
+    # URL to the Class Based Built In Login / Logout views :
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+
+    # Change Password URL
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+
+     # Dashboard URL
+    path("", views.dashboard, name="dashboard"),
 ]
