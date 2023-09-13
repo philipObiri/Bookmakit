19eaec29f5adc6a7e7e7311946fa9ab96018ee7d
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-13 23:53:36 +0000

    Added User Profile Edit View

--- account/views.py
+++ account/views.py
@@ -59,22 +59,25 @@ def register(request):
 
 @login_required
 def edit(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         user_form = UserEditForm(instance=request.user,
-        data=request.post)
-
+                                 data=request.POST)
         profile_form = ProfileEditForm(
-            instance = request.user.profile,
-            data=request.POST,
-            files=request.FILES,
-        )
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                      'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance = request.user.profile)
-    
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile)
     return render(request,
-    'account/edit.html',
-    {'user_form': user_form,'profile_form': profile_form})
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
