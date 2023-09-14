5d6dcab2fa2157ce1e5899ba1ab281e70cef7ea9
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-14 12:35:28 +0000

    feat: Added custom Authentication to the Backend

--- account/authentication.py
+++ account/authentication.py
@@ -16,8 +16,8 @@ class EmailAuthBackend:
             return None
 
 
-def get_user(self, user_id):
-    try:
-        return User.objects.get(pk=user_id)
-    except User.DoesNotExist:
-        return None
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
