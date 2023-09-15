515aebd60a5aef7136649a70d45f1140ac04ad02
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-15 00:59:44 +0000

    Added Python Social Authentication

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Third Party Packages:
+    'social_django',
 ]
 
 MIDDLEWARE = [
