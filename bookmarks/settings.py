5839f41b264ddadd9c3e2e89621486377efe9720
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-15 02:07:28 +0000

    Setting Environment Variables

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z8bs)t8z)^s9)@3!0j@16ovdf6)#f70vrh$r$-y!$--icyk4wl'
+SECRET_KEY = os.environ.get(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
