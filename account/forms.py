49c3f7b0642803a53ec75acc9ef2f1c59214e305
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-28 16:50:34 +0000

    feat: Set Up forms for User Registration, User Login and User Profile Edits

--- account/forms.py
+++ account/forms.py
@@ -1,37 +1,29 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Profile 
+from .models import Profile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(widget= forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
 
 
-# Registration
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password',
-    widget=forms.PasswordInput)
+                               widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password',
-    widget=forms.PasswordInput)
+                                widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
 
-    """
-    compare the second password against the first one
-    and raise a validation error if the passwords don’t match 
-    """
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
-    
-    """ 
-    Validation for the email field that prevents users from registering with an existing
-    email address.
-    """
+
     def clean_email(self):
         data = self.cleaned_data['email']
         if User.objects.filter(email=data).exists():
@@ -39,21 +31,11 @@ class UserRegistrationForm(forms.ModelForm):
         return data
 
 
-
-"""
-Allow users to edit their first name, last name, and email, which are
-attributes of the built-in Django User model
-"""
 class UserEditForm(forms.ModelForm):
     class Meta:
-        model = User 
+        model = User
         fields = ['first_name', 'last_name', 'email']
 
-    
-    """
-    Validation for the email field that prevents users from changing / updating their 
-    existing email address to an existing email address of another user.
-    """
     def clean_email(self):
         data = self.cleaned_data['email']
         qs = User.objects.exclude(id=self.instance.id)\
@@ -63,12 +45,6 @@ class UserEditForm(forms.ModelForm):
         return data
 
 
-"""
-Allow users to edit the profile data that is saved in the Custom
-Profile model extending the built in User Model. 
-Users will be able to edit their date of birth and upload an image for their
-profile picture.
-"""
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
