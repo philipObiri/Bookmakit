826bac61c7b39abd8dfacef3a0437cd1bc8e1b0c
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-28 16:56:04 +0000

    feat: Setting up URLs

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -15,19 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
-# Handling Media Files 
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('social-auth/',
+         include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
 ]
 
-
-# Configuring the django media server to serve media and static files in locally.
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
